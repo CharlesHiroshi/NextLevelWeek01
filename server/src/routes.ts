1f1e99a72947ccb40daaa9fc10860b5179456677
Charles Hiroshi <charles_hiroshi@outlook.com>
2020-06-21 18:57:02 +0900

    Create List Items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,21 @@
-import express from 'express';
+import express from 'express'
+import knex from './database/connection'
 
-const routes = express.Router();
+const routes = express.Router()
 
 routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
+  return response.json({ message: 'Hello World' })
+})
 
-export default routes;
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*')
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    }
+  })
+  return response.json(serializedItems)
+})
+
+export default routes
