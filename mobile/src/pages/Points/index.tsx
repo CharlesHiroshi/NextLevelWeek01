def7805bf332490f21558da272a78fee24c87a6e
Charles Hiroshi <charles_hiroshi@outlook.com>
2020-07-11 23:55:02 +0900

    End of App

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image, SafeAreaView, Alert } from 'react-native'
 import { Feather as Icon } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import MapView, { Marker } from 'react-native-maps'
 import { SvgUri } from 'react-native-svg'
 import * as Location from 'expo-location'
@@ -21,6 +21,10 @@ interface Point {
   longitude: number
 }
 
+interface Params {
+  uf: string
+  city: string
+}
 
 const Points = () => {
   const [items, setItems] = useState<Item[]>([])
@@ -28,6 +32,8 @@ const Points = () => {
   const [selectedItems, setSelectedItems] = useState<number[]>([])
   const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0])
   const navigation = useNavigation()
+  const route = useRoute()
+  const routeParams = route.params as Params
 
   useEffect(() => {
     async function loadPosition() {
@@ -52,14 +58,14 @@ const Points = () => {
   useEffect(() => {
     api.get('points', {
       params: {
-        city: 'Dourados',
-        uf: 'MS',
-        items: [3,]
+        city: routeParams.city,
+        uf: routeParams.uf,
+        items: selectedItems
       }
     }).then(response => {
       setPoints(response.data)
     })
-  }, [])
+  }, [selectedItems])
 
   function handleNavigateBack() {
     navigation.goBack()
