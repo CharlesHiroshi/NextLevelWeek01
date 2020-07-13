78eb3273b2a5699a5e97474a988e8da915ba3e49
Charles Hiroshi <charles_hiroshi@outlook.com>
2020-07-13 20:22:55 +0900

    Altera o função de carregar a imagem

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,5 +10,6 @@ app.use(express.json())
 app.use(routes)
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use('/uploads_users', express.static(path.resolve(__dirname, '..', 'uploads_users')))
 
 app.listen(3333)
